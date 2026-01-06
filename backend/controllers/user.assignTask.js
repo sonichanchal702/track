cc26e6032cea9920430281dd2a47aa32136ea704
Gourav004 <gouravthakurpp@gmail.com>
2026-01-06 16:54:21 +0530

    created project and assignTeask API

---
+++ backend/controllers/user.assignTask.js
@@ -0,0 +1,46 @@
+import Team from "../models/team.schema.js";
+import Project from "../models/project.schema.js";
+
+export const assignMember = async (req, res) => {
+  try {
+    const { id } = req.params; //param me projectID aayegi
+    const agencyId = req.user._id; //token
+    const { teamId } = req.body;
+
+    if (!teamId) {
+      return res.status(400).send("Team id is missing");
+    }
+
+    const project = await Project.findOne({
+      _id: id,
+      createdBy: agencyId,
+    });
+    if (!project) {
+      return res.status(400).send("No project found");
+    }
+    const team = await Team.findOne({
+      _id: teamId,
+      agencyId,
+    });
+
+    if (!team) {
+      return res.status(400).send("No team found.");
+    }
+
+    //assign
+    //project m chanegs
+    project.assignedTo = team._id;
+    project.status = "active";
+    await project.save();
+
+    //team me chanegs
+    (team.status = "busy"), await team.save();
+
+    return res.status(200).json({
+      message: "Freelancer assigned successfully",
+      project,
+    });
+  } catch (error) {
+    return res.status(401).send("ERROR OCCURED: " + error.message);
+  }
+};
--- backend/routes/user.auth.js
+++ backend/routes/user.auth.js
@@ -4,19 +4,21 @@ import { userAuth } from "../middlewares/user.middleware.js";
 import { addTeamMember } from "../controllers/addTeamMember.js";
 import { viewTeam } from "../controllers/user.getTeam.js";
 import { editTeam } from "../controllers/editTeam.js";
-import { getTeamById} from "../controllers/user.getTeamById.js";
-import {deleteTeamById} from "../controllers/user.deleteTeamById.js";
-
+import { getTeamById } from "../controllers/user.getTeamById.js";
+import { deleteTeam } from "../controllers/user.deleteTeamById.js";
+import { createProject } from "../controllers/project.create.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+//team routes
 router.post("/add-new-member", userAuth, addTeamMember);
 router.get("/view-team", userAuth, viewTeam);
 router.patch("/edit-team/:id", userAuth, editTeam);
 router.get("/team/:id", userAuth, getTeamById);
-router.delete("/team/:id", userAuth, deleteTeamById);
-
+router.delete("/team/:id", userAuth, deleteTeam);
+//project routes
+router.post("/createProject", userAuth, createProject);
 export default router;
