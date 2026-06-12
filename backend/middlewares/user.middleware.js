8c3ce072a4ca998757481b951011280b78c8e553
sonichanchal702 <sonichanchal702@gmail.com>
2026-06-12 10:43:18 +0530

    Updating the folders

--- backend/middlewares/user.middleware.js
+++ backend/middlewares/user.middleware.js
@@ -3,6 +3,9 @@ import User from "../models/user.schema.js";
 
 export const userAuth = async (req, res, next) => {
   try {
+    console.log("Cookies:", req.cookies);
+    console.log("Cookie Token:", req.cookies.token);
+
     const token = req.cookies.token;
 
     if (!token) {
@@ -10,8 +13,10 @@ export const userAuth = async (req, res, next) => {
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("Decoded:", decoded);
 
     const agency = await User.findById(decoded.id);
+
     if (!agency) {
       return res.status(404).send("Agency not found");
     }
@@ -19,6 +24,31 @@ export const userAuth = async (req, res, next) => {
     req.user = agency;
     next();
   } catch (error) {
+    console.log(error);
     return res.status(401).send("ERROR OCCURED: " + error.message);
   }
 };
+
+// export const userAuth = async (req, res, next) => {
+  // try {
+    // const token = req.cookies.token;
+// 
+// 
+    // if (!token) {
+      // return res.status(401).send("Token is not present (in Middleware).");
+    // }
+// 
+    // const decoded = jwt.verify(token, process.env.JWT_SECRET);
+// 
+    // const agency = await User.findById(decoded.id);
+    // if (!agency) {
+      // return res.status(404).send("Agency not found");
+    // }
+// 
+    // req.user = agency;
+    // next();
+  // } catch (error) {
+    // return res.status(401).send("ERROR OCCURED: " + error.message);
+  // }
+// };
+// 
